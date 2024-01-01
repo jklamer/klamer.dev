eb3c286a045a50dd15590f8e10912ca71a4e2654
Jack Klamer <jfklamer@gmail.com>
2023-12-31 20:49:35 -0600

    Nav and css

--- src/html.rs
+++ src/html.rs
@@ -1,14 +1,6 @@
 use axum::response::Html;
 use derive_builder::Builder;
 
-pub fn html_component<'a>(component : &str, properties: &[impl AsRef<str>]) -> String {
-    let mut component = "<".to_string() + component + " ";
-    for p in properties.iter() {
-        component += (*p).as_ref();
-    }
-    component + " />"
-}
-
 #[derive(Clone, Builder, Default)]
 pub struct Attributes {
     #[builder(setter(each(name = "htmx_attribute")), default = "vec![]")]
@@ -17,21 +9,43 @@ pub struct Attributes {
     pub attributes: Vec<Attribute>,
 }
 
+fn html_element_with_attributes<'a>(component : &str, custom_attributes: Option<Vec<String>>, attributes: &Option<Attributes>) -> String {
+    let mut component = format!("<{} ", component);
+    if let Some(custom_attr) = custom_attributes {
+        component += custom_attr.join(" ").as_str();
+    }
+
+    if let Some(a) = attributes {
+        for h in a.htmx_attributes.iter() {
+            component += format!(" {}", h.to_string().as_str()).as_str();
+        }
+        for a in a.attributes.iter() {
+            component += format!(" {}", a.to_string().as_str()).as_str();
+        }
+    }
+    component + " />"
+}
+
 
 #[derive(Clone)]
 pub(crate) enum Attribute {
-    HREF(String),
+    CLASS(Vec<String>),
+    WIDTH(u32),
+    HEIGHT(u32),
 }
 
 impl Attribute {
     fn to_string(&self) -> String {
         match self {
-            Attribute::HREF(s) => "href=\"".to_string() + s + "\"",
+            Attribute::CLASS(s) => format!("class=\"{}\"", s.join(" ")),
+            Attribute::WIDTH(u) => format!("width=\"{}\"", u),
+            Attribute::HEIGHT(u) => format!("height=\"{}\"", u),
         }
     }
 }
 
 #[derive(Clone)]
+#[allow(dead_code)]
 pub(crate) enum HtmxAttributes {
     GET(String),
     POST(String),
@@ -59,27 +73,48 @@ pub trait IntoHtml {
     fn html_string(&self) -> String;
 }
 
+impl<T>  From<T> for Box<dyn IntoHtml>
+where T : IntoHtml + 'static
+{
+    fn from(value: T) -> Self {
+        Box::new(value)
+    }
+}
+
+impl IntoHtml for String {
+    fn html_string(&self) -> String {
+        self.clone()
+    }
+}
+
+impl IntoHtml for &str {
+    fn html_string(&self) -> String {
+        self.to_string()
+    }
+}
 
 #[derive(Clone)]
 pub struct Header(pub String);
 
 impl IntoHtml for Header {
     fn html_string(&self) -> String {
-        html_component("h1", &[" "]) + self.0.as_str() + "</h1>"
+        html_element_with_attributes("h1", None, &None) + self.0.as_str() + "</h1>"
     }
 }
 
+#[derive(Builder)]
+#[builder(pattern = "owned")]
 pub struct Div {
-    pub components: Vec<Box<dyn IntoHtml>>,
-    pub attributes: Attributes,
+    #[builder(setter(each(name = "element", into)), default = "vec![]")]
+    pub elements: Vec<Box<dyn IntoHtml>>,
+    #[builder(setter(into, strip_option), default)]
+    pub attributes: Option<Attributes>,
 }
 
 impl IntoHtml for Div {
     fn html_string(&self) -> String {
-        let mut s = html_component("div", &self.attributes.htmx_attributes.iter().map(|x| x.to_string())
-            .chain(self.attributes.attributes.iter().map(|x| x.to_string()))
-            .collect::<Vec<String>>().as_slice());
-        for i in self.components.iter() {
+        let mut s = html_element_with_attributes("div", None, &self.attributes);
+        for i in self.elements.iter() {
             s += i.html_string().as_str();
         }
         s += "</div>";
@@ -87,29 +122,76 @@ impl IntoHtml for Div {
     }
 }
 
+pub struct SimpleDiv<T:IntoHtml>(pub T);
+
+impl<T:IntoHtml> IntoHtml for SimpleDiv<T> {
+    fn html_string(&self) -> String {
+        html_element_with_attributes("div", None, &None) + self.0.html_string().as_str() + "</div>"
+    }
+}
+
 pub struct Anchor<T: IntoHtml>(pub String, pub T);
 
 impl<T: IntoHtml> IntoHtml for Anchor<T> {
     fn html_string(&self) -> String {
-        html_component("a",
-                       &["href=\"".to_string() + self.0.as_str() + "\"",
-                        "style=\"text-decoration:none;color:inherit;\"".to_string()])
-            .to_string()
+        html_element_with_attributes("a",
+                                     Some(vec![
+                                         format!("href=\"{}\"", self.0),
+                                         "style=\"text-decoration:none;color:inherit;\"".to_string()]),
+                                     &None)
             + self.1.html_string().as_str()
             + "</a>"
     }
 }
 
+#[derive(Builder)]
+#[builder(pattern = "owned")]
 pub struct Img {
     pub uri: String,
     pub alt_text: String,
+    #[builder(setter(into, strip_option), default)]
+    pub attributes: Option<Attributes>,
 }
 
 impl IntoHtml for Img {
     fn html_string(&self) -> String {
-        html_component("img",
-                       &["src=\"".to_string() + self.uri.as_str() + "\"",
-                                "alt=\"".to_string() + self.alt_text.as_str() + "\""])
+        html_element_with_attributes("img",
+                                     Some(vec![format!("src=\"{}\"", self.uri.as_str()),
+                                               format!("alt=\"{}\"", self.alt_text.as_str())]),
+                                     &self.attributes)
         + "</img>"
+    }
+}
+
+#[derive(Builder)]
+#[builder(pattern = "owned")]
+pub struct Ulist {
+    #[builder(setter(each(name = "item", into)), default = "vec![]")]
+    pub items: Vec<Box<dyn IntoHtml>>,
+    #[builder(setter(into, strip_option), default)]
+    pub attributes: Option<Attributes>,
+    #[builder(setter(into, strip_option), default)]
+    pub item_attributes: Option<Attributes>,
+}
+
+
+// for posterity this was completely AI generated. WTF
+impl IntoHtml for Ulist{
+    fn html_string(&self) -> String {
+        let mut s = html_element_with_attributes("ul", None, &self.attributes);
+        for i in self.items.iter() {
+            s += html_element_with_attributes("li", None, &self.item_attributes).as_str();
+            s += i.html_string().as_str();
+            s += "</li>";
+        }
+        s + "</ul>"
+    }
+}
+
+pub struct Hr;
+
+impl IntoHtml for Hr {
+    fn html_string(&self) -> String {
+        html_element_with_attributes("hr", None, &None)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-use axum::body::Body;
-use axum::handler::Handler;
-use axum::http::header::CONTENT_TYPE;
-use axum::http::HeaderValue;
-use axum::response::{Html, IntoResponse, Response};
+use axum::response::Html;
 use axum::Router;
 use axum::routing::get;
-use futures::executor;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber;
 
-use crate::html::{Anchor, Header, Img, IntoHtml};
+use crate::html::{Anchor, AttributesBuilder, DivBuilder, Hr, ImgBuilder, IntoHtml, UlistBuilder};
+use crate::html::Attribute::{CLASS, WIDTH};
 
 mod html;
 
 const ICON: &[u8] = include_bytes!("../assests/k_logo.dev.png");
 const LOGO: &[u8] = include_bytes!("../assests/klamer.dev.png");
+const BASE_CSS: &str = include_str!("../css/base.css");
 
 #[tokio::main]
 async fn main() {
@@ -24,6 +21,7 @@ async fn main() {
         .route("/blog", get(blog_page))
         .route("/favicon.png", get(icon))
         .route("/logo.png", get(logo))
+        .route("/base.css", get(base_css))
         .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -34,50 +32,74 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-fn include_htmx(response: Response<Body>) -> Response<Body> {
-    if response.headers().get(CONTENT_TYPE) == Some(&HeaderValue::from_static("text/html; charset=utf-8")) {
-        let mut body = executor::block_on_stream(response.into_body().into_data_stream());
-        let mut bytes = Vec::new();
-        while let Some(frame) = body.next() {
-            bytes.extend_from_slice(&frame.unwrap());
-        }
-        bytes.extend_from_slice(b"\n<script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script>");
-        //let bytes = Bytes::from(bytes);
-        Html(bytes).into_response()
-    } else {
-        response
-    }
-}
-
 async fn home_page() -> Html<String> {
-    page(vec![
-        Box::new(Img {
-            uri: "/logo.png".to_string(),
-            alt_text: "Klamer.dev logo".to_string()
-        }),
-        Box::new(Anchor("/blog".to_string(), Header("Hello world".to_string())))
-    ])
+    page(vec!["Home content".into()])
 }
 
 // write axum handlers needed to set up a blog
 async fn blog_page() -> Html<String> {
-    page(vec![
-            Box::new(Anchor("/".to_string(), Header("Blog".to_string())))
-    ])
+    page(vec!["Blog content".into()])
 }
 
-fn page(components: Vec<Box<dyn IntoHtml>>) -> Html<String> {
+fn page(mut content: Vec<Box<dyn IntoHtml>>) -> Html<String> {
+    //content.insert(0, Box::new(Hr));
+    let page_components: Vec<Box<dyn IntoHtml>> = vec![
+        Box::new(DivBuilder::default()
+            .element(Anchor("/".to_string(), ImgBuilder::default()
+                .uri("/logo.png".to_string())
+                .alt_text("Klamer.dev logo".to_string())
+                .attributes(AttributesBuilder::default()
+                    .attribute(WIDTH(150))
+                    .build().unwrap())
+                .build().unwrap()))
+            .attributes(AttributesBuilder::default()
+                .attribute(CLASS(vec!["Logo".to_string()]))
+                .build().unwrap())
+            .build().unwrap()),
+        Box::new(
+            DivBuilder::default()
+                .element(UlistBuilder::default()
+                    .item(Anchor("/".to_string(), "Home"))
+                    .item(Anchor("/blog".to_string(), "Blog"))
+                    .attributes(AttributesBuilder::default()
+                        .attribute(CLASS(vec!["section-items".to_string()]))
+                        .build().unwrap())
+                    .item_attributes(AttributesBuilder::default()
+                        .attribute(CLASS(vec!["section-item".to_string()]))
+                        .build().unwrap())
+                    .build().unwrap())
+                .attributes(AttributesBuilder::default()
+                    .attribute(CLASS(vec!["Sections".to_string()]))
+                    .build().unwrap())
+                .build().unwrap()
+        ),
+        Box::new(
+            DivBuilder::default()
+                .elements(content)
+                .attributes(AttributesBuilder::default()
+                    .attribute(CLASS(vec!["Content".to_string()]))
+                    .build().unwrap())
+                .build().unwrap()
+        )
+    ];
     Html("<html>".to_string()
         + "<head>
           <title>Klamer.dev</title>
           <link rel=\"icon\" type=\"image/png\" href=\"/favicon.png\">
+          <link rel=\"stylesheet\" href=\"/base.css\">
           <script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script>
           </head>"
         + "<body>"
-        + components.iter().map(|x| x.html_string()).collect::<Vec<String>>().join("").as_str()
+        + DivBuilder::default()
+        .elements(page_components)
+        .attributes(AttributesBuilder::default()
+            .attribute(CLASS(vec!["Container".to_string(),"center".to_string()]))
+            .build().unwrap()).build().unwrap().html_string().as_str()
         + "</body>"
         + "</html>")
 }
+
+
 
 
 async fn icon() -> &'static [u8] {
@@ -86,4 +108,8 @@ async fn icon() -> &'static [u8] {
 
 async fn logo() -> &'static [u8] {
     LOGO
+}
+
+async fn base_css() -> &'static str {
+    BASE_CSS
 }
